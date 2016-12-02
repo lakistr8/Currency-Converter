faad326c1e8fb55a1801c211f2710f81dc6cb020
Lazar <lakiando@outlook.com>
2016-12-02 18:21:28 +0100

    Added new file, CocoaTouch. Called it CurrencyBox, subclass of UIView
    Connected with cur left and right

---
+++ CurrencyConverter/CurrencyBox.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyBox.swift
+//  CurrencyConverter
+//
+//  Created by iosakademija on 12/2/16.
+//  Copyright © 2016 iosakademija. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyBox: UIView {
+
+}
