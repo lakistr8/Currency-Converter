624753e9c68928532c93e2a7a52b9be099176370
Lazar <lakiando@outlook.com>
2016-12-02 17:45:56 +0100

    added new file CurrencyControler.swift witch is sub class of view controller

---
+++ CurrencyConverter/CurrencyControler.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyConcerter.swift
+//  CurrencyConverter
+//
+//  Created by iosakademija on 12/2/16.
+//  Copyright © 2016 iosakademija. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyControler: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
