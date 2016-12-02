acd96faf651a41fc3fabda34282a39c12253a90b
Lazar <lakiando@outlook.com>
2016-12-02 18:09:30 +0100

    Configured target-action for the button touches
    created 2 new extensions and inside of them put view did load and button touches

--- CurrencyConverter/CurrencyControler.swift
+++ CurrencyConverter/CurrencyControler.swift
@@ -39,13 +39,61 @@ class CurrencyControler: UIViewController {
     func digitButtonTapped(_ sender: UIButton) {
         
     }
+}
 
+
+
+typealias UISetup = CurrencyControler
+extension UISetup {
+    
+    func assignButtonTargets() {
+        let allButtons = digitButtons + operatorButtons + [decimalButton, deleteButton]
+        for btn in allButtons {
+            btn.addTarget(self, action: #selector(CurrencyControler.didTouchButton(_:)), for: .touchDown)
+            
+            btn.addTarget(self, action: #selector(CurrencyControler.didUntouchButton(_:)), for: .touchCancel)
+            btn.addTarget(self, action: #selector(CurrencyControler.didUntouchButton(_:)), for: .touchUpOutside)
+        }
+    }
+    
+    func didTouchButton(_ sender: UIButton) {
+        //		guard let bgColor = sender.backgroundColor else { return }
+        //		originalBackgroundColor = bgColor
+        //		let newColor = bgColor.withAlphaComponent(0.96)
+        //		sender.backgroundColor = newColor
+    }
+    
+    func didUntouchButton(_ sender: UIButton) {
+        //		guard let bgColor = originalBackgroundColor else { return }
+        //		sender.backgroundColor = bgColor
+        //		originalBackgroundColor = nil
+    }
+    
+    func configureDecimalButton() {
+        let locale = NSLocale.current
+        guard let decimalSign = locale.decimalSeparator else { return }
+        decimalButton.setTitle(decimalSign, for: .normal)
+    }
+    
+}
+
+
+
+
+typealias ViewLifecycle = CurrencyControler
+extension ViewLifecycle {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.title = self.navigationItem.title?.uppercased()
+        
+        assignButtonTargets()
+        configureDecimalButton()
     }
 }
+
+
 
 extension CurrencyControler : UITextFieldDelegate {
     
