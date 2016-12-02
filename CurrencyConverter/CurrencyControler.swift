cd1d72e2c98e3434215f254b616772c553def9ef
Lazar <lakiando@outlook.com>
2016-12-02 18:19:40 +0100

    Temporary comment out

--- CurrencyConverter/CurrencyControler.swift
+++ CurrencyConverter/CurrencyControler.swift
@@ -120,6 +120,7 @@ extension ViewLifecycle {
         self.title = self.navigationItem.title?.uppercased()
         
         assignButtonTargets()
+        assignButtonTaps()
         configureDecimalButton()
         cleanupUI()
     }
@@ -139,83 +140,83 @@ extension Internal {
     }
     
     func digitButtonTapped(_ sender: UIButton) {
-        guard let numString = sender.title(for: .normal) else { return }
-        var value = resultField.text ?? ""
-        
-        defer {
-            self.didUntouchButton(sender)
-        }
-        
-        value += numString
-        resultField.text = value
+//        guard let numString = sender.title(for: .normal) else { return }
+//        var value = resultField.text ?? ""
+//
+//        defer {
+//            self.didUntouchButton(sender)
+//        }
+//        
+//        value += numString
+//        resultField.text = value
     }
     
     func operationButtonTapped(_ sender: UIButton) {
-        var isEquals = false
-        
-        
-        guard let caption = sender.title(for: .normal) else {
-            fatalError("Received operator button tap rom button with no caption on it")
-        }
-        
-        
-        switch caption {
-        case "+":
-            activeOperation = .add
-        case "-":
-            activeOperation = .subtract
-        case "×":
-            activeOperation = .multiply
-        case "÷":
-            activeOperation = .divide
-        case "=":
-            isEquals = true
-        default:
-            activeOperation = .none
-        }
-        
-        if (isEquals) {
-            
-            guard let num = validateOperandInput() else {
-                resultField.text = nil
-                return
-            }
-            secondOperand = num
-            
-            
-            var rez = firstOperand
-            switch activeOperation {
-            case .add:
-                rez += secondOperand
-            case .subtract:
-                rez -= secondOperand
-            case .multiply:
-                rez *= secondOperand
-            case .divide:
-                rez /= secondOperand
-            default:
-                break
-            }
-            
-            resultField.text = formatter.string(for: rez)
-            
-            
-            firstOperand = 0
-            secondOperand = 0
-            
-        } else if activeOperation != .none {
-            
-            
-            guard let num = validateOperandInput() else {
-                resultField.text = nil
-                return
-            }
-            firstOperand = num
-            
-            resultField.text = nil
-        }
-        
-        self.didUntouchButton(sender)
+//        var isEquals = false
+//        
+//        
+//        guard let caption = sender.title(for: .normal) else {
+//            fatalError("Received operator button tap rom button with no caption on it")
+//        }
+//        
+//        
+//        switch caption {
+//        case "+":
+//            activeOperation = .add
+//        case "-":
+//            activeOperation = .subtract
+//        case "×":
+//            activeOperation = .multiply
+//        case "÷":
+//            activeOperation = .divide
+//        case "=":
+//            isEquals = true
+//        default:
+//            activeOperation = .none
+//        }
+//        
+//        if (isEquals) {
+//            
+//            guard let num = validateOperandInput() else {
+//                resultField.text = nil
+//                return
+//            }
+//            secondOperand = num
+//            
+//            
+//            var rez = firstOperand
+//            switch activeOperation {
+//            case .add:
+//                rez += secondOperand
+//            case .subtract:
+//                rez -= secondOperand
+//            case .multiply:
+//                rez *= secondOperand
+//            case .divide:
+//                rez /= secondOperand
+//            default:
+//                break
+//            }
+//            
+//            resultField.text = formatter.string(for: rez)
+//            
+//            
+//            firstOperand = 0
+//            secondOperand = 0
+//            
+//        } else if activeOperation != .none {
+//            
+//            
+//            guard let num = validateOperandInput() else {
+//                resultField.text = nil
+//                return
+//            }
+//            firstOperand = num
+//            
+//            resultField.text = nil
+//        }
+//        
+//        self.didUntouchButton(sender)
     }
 }
 
