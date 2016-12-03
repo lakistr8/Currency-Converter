802a749af10f05b507da0cfa5f2259722a9e0a35
Lazar <lakiando@outlook.com>
2016-12-03 11:30:51 +0100

    Added CurrencyCell as subclass of UITableViewCell

---
+++ CurrencyConverter/CurrencyCell.swift
@@ -0,0 +1,24 @@
+//
+//  CurrencyCell.swift
+//  CurrencyConverter
+//
+//  Created by iosakademija on 12/3/16.
+//  Copyright © 2016 iosakademija. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
